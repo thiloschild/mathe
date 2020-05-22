b2aac3f9c8da93397f6641911f84af4906d048fb
Fabian Steinfeld <fabian.steinfeld.99@gmail.com>
2020-05-22 22:11:42 +0200

    hallo

--- mathe.py
+++ mathe.py
@@ -11,12 +11,22 @@ list1 = []
 num_col = table.ncols
 
 for x in range(1, num_col):
-	list1.append(table.cell(1, x).value)
+    list1.append(table.cell(1, x).value)
 
 median = np.median(list1)
 mean = np.mean(list1)
+variance = np.var(list1)
+std = np.std(list1)
+corrcoef = np.corrcoef(list1)
 
+print(median)
 print(mean)
+print(variance)
+print(std)
+print(corrcoef)
+
+
+
 
 
 
@@ -26,9 +36,13 @@ print(list1)
 #############################################
 #write to excel
 
-wb = openpyx.load_workbook("exam.xlsx")
-sh = wb.get_sheet_by_name("sol")
+wb = openpyxl.load_workbook("exam.xlsx")
+sh = wb["sol"]
+dest_filename = "exam.xlsx"
 
-sh['B1'] = 6
+cell = sh.cell(2,2)
+cell.value = median
 
-wb.save()
+wb.save(filename=dest_filename)
+
+
