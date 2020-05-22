27d2afb1ee61e4d6169492eaeb73de773ef58651
ThiloSchild <thilo_schild@me.com>
2020-05-22 18:04:20 +0200

    added code

--- mathe.py
+++ mathe.py
@@ -2,4 +2,15 @@ import pandas as pd
 import numpy as np
 import xlrd
 
-a = 1000
+csv = xlrd.open_workbook("exam.xlsx")
+table = csv.sheet_by_name("table")
+
+list1 = []
+
+num_col = table.ncols
+
+for x in num_col:
+	list1 = table.cell(1, x)
+
+
+print(list1)
