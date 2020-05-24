22f98936226b1571f991ac375fe3509cc8f98a66
Fabian Steinfeld <fabian.steinfeld.99@gmail.com>
2020-05-24 02:06:28 +0200

    it works

--- 2variables.py
+++ 2variables.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xlwings as xw
 
+#count the horizontal columns in the excel sheet
 def countcol(row_num):
     lastcol = 0
     while True:
@@ -12,26 +13,57 @@ def countcol(row_num):
             break
     return lastcol +1
 
+def cova(x,y):
+    meanxy = 0
+    lenx = len(x)
+    for i in range(lenx):
+        meanxy += x[i] * y[i]
+    cov = meanxy / lenx - (meanx * meany)
+    return cov
 
-wb = xw.Book("2variable.xlsm")
+def corrcoef(x,y):
+    cov = cova(x,y)
+    stdax = np.std(x)
+    stday = np.std(y)
+    corrcoef = cov / (stdax * stday)
+    return corrcoef
+
+
+wb = xw.Book("2variables.xlsm")
 
 num_col = countcol(2)
-print(num_col)
-list1 = []
 
-for x in range(2, num_col):
-    list1.append(xw.Range((2,x)).value)
+x = []
+y = []
 
-print(list1)
+for i in range(2, num_col):
+    x.append(xw.Range((2,i)).value)
 
-median = np.median(list1)
-mean = np.mean(list1)
-variance = np.var(list1)
-stda = np.std(list1)
-varcoef = stda/np.abs(mean)
+for i in range(2, num_col):
+    y.append(xw.Range((3,i)).value)
 
-xw.Range("B5").value = median
-xw.Range("B6").value = mean
-xw.Range("B7").value = variance
-xw.Range("B8").value = stda
-xw.Range("B9").value = varcoef
+medianx = np.median(x)
+mediany = np.median(y)
+meanx = np.mean(x)
+meany = np.mean(y)
+variancex = np.var(x)
+variancey = np.var(y)
+stdax = np.std(x)
+stday = np.std(y)
+varcoefx = stdax/np.abs(meanx)
+varcoefy = stday/np.abs(meany)
+cov = cova(x,y)
+corrcoef = corrcoef(x,y)
+
+xw.Range("B6").value = medianx
+xw.Range("C6").value = mediany
+xw.Range("B7").value = meanx
+xw.Range("C7").value = meany
+xw.Range("B8").value = variancex
+xw.Range("C8").value = variancey
+xw.Range("B9").value = stdax
+xw.Range("C9").value = stday
+xw.Range("B10").value = varcoefx
+xw.Range("C10").value = varcoefy
+xw.Range("B11").value = cov
+xw.Range("B12").value = corrcoef
