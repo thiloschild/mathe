90cf8fa87582e0c9b6902bd32b107aa224190b77
ThiloSchild <thilo_schild@me.com>
2020-05-24 17:57:31 +0200

    test

--- 4felder.py
+++ 4felder.py
@@ -31,24 +31,54 @@ def countrow(col_num):
             break
     return lastrow +1
 
-
 ##################################################################################
 #import from the excel file
 
 xw.Book('4felder.xlsm')
 lastcol = countcol(1)
 lastrow = countrow(1)
+
+
+
+
+#############
 PvonA = xw.Range((lastrow,2)).value
 PvonB = xw.Range((lastrow,3)).value
 if(lastcol > 3):
     PvonC = xw.Range((lastrow,4)).value
+#############
 PvonX = xw.Range((2,lastcol)).value
 PvonY = xw.Range((3,lastcol)).value
 if(lastrow > 3):
     PvonZ = xw.Range((4,lastcol)).value
+#############
+P_AX = xw.Range((2,2)).value
+P_AY = xw.Range((3,2)).value
+if(lastrow > 3):
+    P_AZ = xw.Range((4,2)).value
+#############
+P_BX = xw.Range((2,3)).value
+P_BY = xw.Range((3,3)).value
+if(lastrow > 3):
+    P_BZ = xw.Range((4,3)).value
+#############
+P_CX = xw.Range((2,4)).value
+P_CY = xw.Range((3,4)).value
+if(lastrow > 3):
+    P_CZ = xw.Range((4,4)).value
+#############
 
 
-print()
+list1 = [PvonA, PvonB, PvonC, PvonX, PvonY, PvonZ, P_AX, P_AY, P_AZ, P_BX, P_BY, P_BZ, P_CX, P_CY, P_CZ]
+list_known = []
+
+for x in list1:
+    if x != None:
+        list_known.append(x)
+
+
+
+print(list_known)
 
 
 
